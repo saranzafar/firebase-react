79c2c91fb04f7e060594bf899cf62feb2cccf280
saranzafar <saran.development@gmail.com>
2024-12-18 00:38:11 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAacuIpGYOm3EU0mA9YfptKTRdLBtYJtck",
+    authDomain: "app-6f096.firebaseapp.com",
+    projectId: "app-6f096",
+    storageBucket: "app-6f096.firebasestorage.app",
+    messagingSenderId: "1078550867685",
+    appId: "1:1078550867685:web:fcd47b1b6e1741a557ae0c",
+    databaseURL: "https://app-6f096-default-rtdb.firebaseio.com/",
+};
+
+export const app = initializeApp(firebaseConfig);
